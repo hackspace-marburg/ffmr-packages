0ce63e526fdf75a5f2dac2e2e62f5d90af4833d9
Oleander Reis <oleander@oleander.cc>
2015-12-06 11:10:17 +0100

    initial commit.

---
+++ gluon-ffmr-yolokey-client/Makefile.mk
@@ -0,0 +1,37 @@
+include $(TOPDIR)/rules.mk
+
+PKG_NAME:=gluon-ffmr-yolokey-client
+PKG_VERSION:=1
+
+PKG_BUILD_DIR := $(BUILD_DIR)/$(PKG_NAME)
+
+include $(INCLUDE_DIR)/package.mk
+
+define Package/gluon-ffmr-yolokey-client
+  SECTION:=gluon
+  CATEGORY:=Gluon
+  TITLE:=Automatic deployment of fastd public keys.
+  DEPENDS:=+fastd
+endef
+
+define Package/gluon-ffmr-yolokey-client/description
+	Automatic deployment of fastd public keys:
+	An on-connect script for fastd uploads the public key to the gateway.
+endef
+
+define Build/Prepare
+	mkdir -p $(PKG_BUILD_DIR)
+endef
+
+define Build/Configure
+endef
+
+define Build/Compile
+endef
+
+define Package/gluon-ffmr-yolokey-client/install
+	$(CP) ./files/* $(1)/
+endef
+
+$(eval $(call BuildPackage,gluon-ffmr-yolokey-client))
+
